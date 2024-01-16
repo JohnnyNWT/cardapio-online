242550675cec454a8f748aad85b89359bf3721a5
JohnnyNWT <jooaaooskt@gmail.com>
2024-01-16 17:07:03 -0300

    FEAT: add arquivos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Olá Mundo!</h1>
+        <Header />
       </div>
     )
   }
