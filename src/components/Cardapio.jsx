2f3f428f0986b08b08ac4520c33c7a7b2a03dad7
JohnnyNWT <jooaaooskt@gmail.com>
2024-01-22 17:46:00 -0300

    FEAT: add icone de carrinho

--- src/components/Cardapio.jsx
+++ src/components/Cardapio.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MENU from '../utils/dados';
+import Header from './Header';
 
 class Cardapio extends Component {
   state = {
@@ -7,6 +8,7 @@ class Cardapio extends Component {
     arrCategoriaCardapio: MENU['burgers'],
     itensExibidos: 8,
     meuCarrinho: [],
+    qntdItensCarrinho: 0,
   };
 
   handleClickRenderCardapio = (event) => {
@@ -14,10 +16,10 @@ class Cardapio extends Component {
 
     const elements = document.querySelectorAll('.container-menu a');
 
-    elements.forEach((element) =>  element.classList.remove('active'))
+    elements.forEach((element) => element.classList.remove('active'))
 
     document.getElementById(`menu-${categoria}`).classList.add('active');
-    
+
     this.setState({
       categoriaItemCardapio: categoria,
       arrCategoriaCardapio: MENU[categoria],
@@ -55,97 +57,112 @@ class Cardapio extends Component {
 
       if (!existe) {
         this.setState((prevState) => ({
-          meuCarrinho: [...prevState.meuCarrinho, {...novoItem, quantidade}],
-        }));
+          meuCarrinho: [...prevState.meuCarrinho, { ...novoItem, quantidade }],
+        }), () => this.atualizarCarrinho());
       } else {
         this.setState((prevState) => ({
-          meuCarrinho: prevState.meuCarrinho.map((e) => e.id === id ? {...e, quantidade: e.quantidade + quantidade} : e),
-        }));
+          meuCarrinho: prevState.meuCarrinho.map((e) => e.id === id ? { ...e, quantidade: e.quantidade + quantidade } : e),
+        }), () => this.atualizarCarrinho());
       }
-      document.getElementById(`qntd-${id}`).textContent = 0
+      document.getElementById(`qntd-${id}`).textContent = 0;
     };
   };
-  
+
+  atualizarCarrinho = () => {
+    const { meuCarrinho } = this.state;
+    let total = 0;
+    meuCarrinho.forEach((e) => total += e.quantidade);
+    this.setState({
+      qntdItensCarrinho: total
+    })
+  };
+
   render() {
-    const { arrCategoriaCardapio, itensExibidos } = this.state;
+    const { arrCategoriaCardapio, itensExibidos, qntdItensCarrinho } = this.state;
     const itensExibidosAtualizados = arrCategoriaCardapio.slice(0, itensExibidos);
     const mostrarBotaoVerMais = itensExibidos < arrCategoriaCardapio.length;
     return (
-      <section className="cardapio" id="cardapio">
-        <div className="background-cardapio"></div>
+      <>
+        <Header qntdItensCarrinho={qntdItensCarrinho} />
+        <a className="botao-carrinho animated bounceIn" onClick="">
+          <div className="badge-total-carrinho">{qntdItensCarrinho}</div>
+          <i className="fa fa-shopping-bag"></i>
+        </a>
+        <section className="cardapio" id="cardapio">
+          <div className="background-cardapio"></div>
+          <div className="container">
+            <div className="row">
 
-        <div className="container">
-          <div className="row">
+              <div className="col-12 col-one text-center mb-5 wow fadeIn">
+                <span className="hint-title"><b>Cardápio</b></span>
+                <h1 className="title">
+                  <b>Conheça o nosso cardápio</b>
+                </h1>
+              </div>
 
-            <div className="col-12 col-one text-center mb-5 wow fadeIn">
-              <span className="hint-title"><b>Cardápio</b></span>
-              <h1 className="title">
-                <b>Conheça o nosso cardápio</b>
-              </h1>
-            </div>
+              <div className="col-12 col-one container-menu wow fadeInUp">
+                <a id="menu-burgers" className="btn btn-white btn-sm mr-3 active"
+                  onClick={this.handleClickRenderCardapio}>
+                  <i id="menu-burgers" className="fas fa-hamburger"></i>&nbsp; Burgers
+                </a>
+                <a id="menu-pizzas" className="btn btn-white btn-sm mr-3" onClick={this.handleClickRenderCardapio}>
+                  <i id="menu-pizzas" className="fas fa-pizza-slice"></i>&nbsp; Pizzas
+                </a>
+                <a id="menu-churrasco" className="btn btn-white btn-sm mr-3"
+                  onClick={this.handleClickRenderCardapio}>
+                  <i id="menu-churrasco" className="fas fa-drumstick-bite"></i>&nbsp; Churrasco
+                </a>
+                <a id="menu-steaks" className="btn btn-white btn-sm mr-3" onClick={this.handleClickRenderCardapio}>
+                  <i id="menu-steaks" className="fas fa-bacon"></i>&nbsp; Steaks
+                </a>
+                <a id="menu-bebidas" className="btn btn-white btn-sm mr-3"
+                  onClick={this.handleClickRenderCardapio}>
+                  <i id="menu-bebidas" className="fas fa-cocktail"></i>&nbsp; Bebidas
+                </a>
+                <a id="menu-sobremesas" className="btn btn-white btn-sm mr-3"
+                  onClick={this.handleClickRenderCardapio}>
+                  <i id="menu-sobremesas" className="fas fa-ice-cream"></i>&nbsp; Sobremesas
+                </a>
+              </div>
 
-            <div className="col-12 col-one container-menu wow fadeInUp">
-              <a id="menu-burgers" className="btn btn-white btn-sm mr-3 active"
-                onClick={this.handleClickRenderCardapio}>
-                <i id="menu-burgers" className="fas fa-hamburger"></i>&nbsp; Burgers
-              </a>
-              <a id="menu-pizzas" className="btn btn-white btn-sm mr-3" onClick={this.handleClickRenderCardapio}>
-                <i id="menu-pizzas" className="fas fa-pizza-slice"></i>&nbsp; Pizzas
-              </a>
-              <a id="menu-churrasco" className="btn btn-white btn-sm mr-3"
-                onClick={this.handleClickRenderCardapio}>
-                <i id="menu-churrasco" className="fas fa-drumstick-bite"></i>&nbsp; Churrasco
-              </a>
-              <a id="menu-steaks" className="btn btn-white btn-sm mr-3" onClick={this.handleClickRenderCardapio}>
-                <i id="menu-steaks" className="fas fa-bacon"></i>&nbsp; Steaks
-              </a>
-              <a id="menu-bebidas" className="btn btn-white btn-sm mr-3"
-                onClick={this.handleClickRenderCardapio}>
-                <i id="menu-bebidas" className="fas fa-cocktail"></i>&nbsp; Bebidas
-              </a>
-              <a id="menu-sobremesas" className="btn btn-white btn-sm mr-3"
-                onClick={this.handleClickRenderCardapio}>
-                <i id="menu-sobremesas" className="fas fa-ice-cream"></i>&nbsp; Sobremesas
-              </a>
-            </div>
-
-            <div className="col-12 col-one">
-              <div className="row" id="itensCardapio">
-                {
-                  itensExibidosAtualizados.map(({ id, img, name, price }) => (
-                    <div className="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 animated fadeInUp">
-                      <div className="card card-item">
-                        <div className="img-produto">
-                          <img src={img} />
-                        </div>
-                        <p className="title-produto text-center mt-4">
-                          <b>{name}</b>
-                        </p>
-                        <p className="price-produto text-center">
-                          <b>R$ {price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</b>
-                        </p>
-                        <div className="add-carrinho">
-                          <span className="btn-menos" onClick={() => this.handleClickDiminuirQuantidade(id)}><i className="fas fa-minus"></i></span>
-                          <span className="add-numero-itens" id={`qntd-${id}`}>0</span>
-                          <span className="btn-mais" onClick={() => this.handleClickAumentarQuantidade(id)}><i className="fas fa-plus"></i></span>
-                          <span className="btn btn-add" onClick={() => this.handleClickAddCarrinho(id)}><i className="fa fa-shopping-bag"></i></span>
+              <div className="col-12 col-one">
+                <div className="row" id="itensCardapio">
+                  {
+                    itensExibidosAtualizados.map(({ id, img, name, price }) => (
+                      <div className="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 animated fadeInUp" key={id}>
+                        <div className="card card-item">
+                          <div className="img-produto">
+                            <img src={img} />
+                          </div>
+                          <p className="title-produto text-center mt-4">
+                            <b>{name}</b>
+                          </p>
+                          <p className="price-produto text-center">
+                            <b>R$ {price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</b>
+                          </p>
+                          <div className="add-carrinho">
+                            <span className="btn-menos" onClick={() => this.handleClickDiminuirQuantidade(id)}><i className="fas fa-minus"></i></span>
+                            <span className="add-numero-itens" id={`qntd-${id}`}>0</span>
+                            <span className="btn-mais" onClick={() => this.handleClickAumentarQuantidade(id)}><i className="fas fa-plus"></i></span>
+                            <span className="btn btn-add" onClick={() => this.handleClickAddCarrinho(id)}><i className="fa fa-shopping-bag"></i></span>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))
-                }
+                    ))
+                  }
+                </div>
               </div>
-            </div>
 
-            <div className={`col-12 col-one text-center wow fadeInUp ${mostrarBotaoVerMais ? '' : 'hidden'}`}>
-              <a id="btnVerMais" className="btn btn-white btn-sm" onClick={this.handleClickVerMais}>
-                Ver mais
-              </a>
-            </div>
+              <div className={`col-12 col-one text-center wow fadeInUp ${mostrarBotaoVerMais ? '' : 'hidden'}`}>
+                <a id="btnVerMais" className="btn btn-white btn-sm" onClick={this.handleClickVerMais}>
+                  Ver mais
+                </a>
+              </div>
 
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </>
     );
   }
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,14 @@ import '../css/fontawesome.css';
 
 class Header extends Component {
   render() {
+    const { qntdItensCarrinho } = this.props;
+
     return (
       <section>
         <div className="container">
           <nav className="navbar navbar-expand-lg pl-0 pr-0">
             <a className="navbar-brand">
-              <img src={ Logo } width="160" className="img-logo" />
+              <img src={Logo} width="160" className="img-logo" />
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
               <span className="navbar-toggler-icon">
@@ -29,9 +31,9 @@ class Header extends Component {
                   <a href="#depoimentos" className="nav-link"><b>Depoimentos</b></a>
                 </li>
               </ul>
-              <a className="btn btn-white btn-icon wow fadeIn" onclick={''}>
+              <a className="btn btn-white btn-icon wow fadeIn">
                 Meu carrinho <span className="icon">
-                  <div className="container-total-carrinho badge-total-carrinho hidden"></div>
+                  <div className="container-total-carrinho badge-total-carrinho">{qntdItensCarrinho}</div>
                   <i className="fa fa-shopping-bag"></i>
                 </span>
               </a>
